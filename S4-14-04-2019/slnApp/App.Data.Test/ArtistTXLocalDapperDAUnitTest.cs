15a900a262d58f9a8a0bdc824ab24fb4147bb066
yiyo25 <ehuapayaromero@gmail.com>
2019-04-28 14:07:26 -0500

    dapper

---
+++ S4-14-04-2019/slnApp/App.Data.Test/ArtistTXLocalDapperDAUnitTest.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using App.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace App.Data.Test
+{
+    [TestClass]
+    public class ArtistTXLocalDapperDAUnitTest
+    {
+        [TestMethod]
+        public void Count()
+        {
+            var da = new ArtistTXLocalDapperDA();
+
+            Assert.IsTrue(da.GetCount()>0);
+        }
+
+        [TestMethod]
+        public void GetAll()
+        {
+            var da = new ArtistTXLocalDapperDA();
+            var listado = da.GetAll();
+            Assert.IsTrue(listado.Count > 0);
+        }
+
+        [TestMethod]
+        public void Get()
+        {
+            var da = new ArtistTXLocalDapperDA();
+            var entity = da.Get(10);
+            Assert.IsTrue(entity.ArtistId > 0);
+        }
+
+        [TestMethod]
+        public void GetAllSP()
+        {
+            var da = new ArtistTXLocalDapperDA();
+            var listado = da.GetAllSP("Aerosmith");
+            Assert.IsTrue(listado.Count > 0);
+        }
+
+        [TestMethod]
+        public void InsertSP()
+        {
+            var da = new ArtistTXLocalDapperDA();
+            var artist = new Artist();
+            artist.Name = "Sebastian";
+            var id = da.Insert(artist);
+            Assert.IsTrue(id > 0,"Nombre del artista ya existe");
+        }
+
+
+        [TestMethod]
+        public void UpdateSP()
+        {
+            var da = new ArtistTXLocalDapperDA();
+            var artist = new Artist
+            {
+                Name = "eduardo",
+                ArtistId = 278
+            };
+           
+            var registrosAfectados = da.Update(artist);
+            Assert.IsTrue(registrosAfectados > 0, "Nombre del artista ya existe");
+        }
+
+        [TestMethod]
+        public void DeleteSP()
+        {
+            var da = new ArtistTXLocalDapperDA();
+           /* var artist = new Artist
+            {
+                ArtistId = 276
+            };*/
+
+            var registrosAfectados = da.DeleteSp(278);
+            Assert.IsTrue(registrosAfectados > 0, "Artista no fue eliminado");
+        }
+    }
+}
---
+++ S4-14-04-2019/slnApp/App.Data/ArtistTXLocalDapperDA.cs
@@ -0,0 +1,125 @@
+﻿using App.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+
+namespace App.Data
+{
+    public class ArtistTXLocalDapperDA:BaseConnection
+    {
+        /// <summary>
+        /// Permite obtener la cantidad de registros que existen en la tabla artista
+        /// </summary>
+        /// <returns>Retorna un número de registros</returns>
+        public int GetCount()
+        {
+            var result = 0;
+            var sql = "Select count(1) from Artist";
+            /*1.- creando la instancia del objeto connection*/
+            using (IDbConnection cn=new SqlConnection(this.ConnectionString))
+            {
+                result = cn.ExecuteScalar<int>(sql);
+            }
+
+
+                return result;
+        }
+
+
+        /// <summary>
+        /// Permite obtener la lista de artistas
+        /// </summary>
+        /// <returns>Lista de artistas</returns>
+        public List<Artist> GetAll(string filterByName = "")
+        {
+            var result = new List<Artist>();
+            var sql = "select * from Artist where Name LIKE '%@paramFilterByName%'";
+
+            using (IDbConnection cn = new SqlConnection(this.ConnectionString))
+            {
+                result = cn.Query<Artist>(sql, new { paramFilterByName = filterByName }).ToList();
+            }
+                return result;
+        }
+
+        public Artist Get(int id)
+        {
+            var result = new Artist();
+            var sql = "select * from Artist where ArtistId = @paramID";
+            using (IDbConnection cn= new SqlConnection(this.ConnectionString))
+            {
+                result = cn.QueryFirstOrDefault<Artist>(sql, new { paramId = id } );
+            }
+
+            return result;
+        }
+
+        public List<Artist> GetAllSP(string filterByName = "")
+        {
+            var result = new List<Artist>();
+            var sql = "ups_GetAll";
+
+            using (IDbConnection cn = new SqlConnection(this.ConnectionString))
+            {
+                result = cn.Query<Artist>(sql,
+                    new { filterByName = filterByName },
+                    commandType:CommandType.StoredProcedure).ToList();
+
+            }
+            return result;
+        }
+
+
+        public int Insert(Artist entity)
+        {
+            var result = 0;
+            using (IDbConnection cn = new SqlConnection(this.ConnectionString))
+            {
+               result = cn.ExecuteScalar<int>("usp_InsertArtist",
+                    new { pName = entity.Name },
+                    commandType: CommandType.StoredProcedure
+                    );
+
+            }
+
+            return result;
+        }
+
+        public int Update(Artist entity)
+        {
+            var result = 0;
+            using (IDbConnection cn = new SqlConnection(this.ConnectionString))
+            {
+                result = cn.Execute("usp_UpdateArtist", 
+                    new {
+                        pName = entity.Name ,
+                        pId = entity.ArtistId
+                    },
+                    commandType:CommandType.StoredProcedure);  
+            }
+
+            return result;
+        }
+
+        public int DeleteSp(int id)
+        {
+            var result = 0;
+            using (IDbConnection cn = new SqlConnection(this.ConnectionString))
+            {
+                result = cn.Execute("usp_DeleteArtist",
+                  new
+                  {
+                      pId = id
+                  },
+                  commandType: CommandType.StoredProcedure);
+
+            }
+                return result;
+        }
+    }
+}
